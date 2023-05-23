a9688fb649e763324100442fa1898cf213e70c84
Wolfpl2001 <112547123+Wolfpl2001@users.noreply.github.com>
2023-05-23 12:49:13 +0200

    Add files via upload
    V2

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+let score = 0;
+
 function computerChoice() {
     const choices = ["paper", "rock", "scissor"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
-  }
-  
-  function checkResult(player, computer) {
+}
+
+
+function checkResult(player, computer) {
     if (player === computer) {
       return "Draw!";
     } else if (
       (player === "paper" && computer === "rock") ||
       (player === "rock" && computer === "scissor") ||
       (player === "scissor" && computer === "paper")
-    ) {
+     ) {
+      score+=1;
       return "You win!";
     } else {
+      if (score > 0) {
+        score-=1;
+      }
       return "You Lose!";
     }
-  }
+}
   
   function playGame(playerChoice) {
     const computerSelection = computerChoice();
@@ -27,5 +34,7 @@ function computerChoice() {
       "<br>Computer choice: " +
       computerSelection +
       "<br><h1 class='text'>Your: " +
-      result;
+      result +
+      "<br> Your score is:" +
+      score;
   }
